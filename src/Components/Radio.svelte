28e03933acc88190e9d4ee6fbf0a5ebf81cd3239
Ling Li <ling@techarge.co.uk>
2021-09-21 16:16:55 +0100

    Allow radio buttons to be displayed horizontally

--- src/Components/Radio.svelte
+++ src/Components/Radio.svelte
@@ -1,75 +1,100 @@
 <script>
-  import { afterUpdate, createEventDispatcher, onMount } from 'svelte';
-  import clsx from 'clsx';
+    import {afterUpdate, createEventDispatcher, onMount} from 'svelte';
+    import clsx from 'clsx';
 
-  // Declar variables.
-  export let field = {};
-  const defaultAttributes = {
-    classes: '',
-  };
-  let classe = null;
-  let defaulClasses = null;
-  export let items = getItems(field);
+    // Declar variables.
+    export let field = {};
+    const defaultAttributes = {
+        classes: '',
+    };
+    let classe = null;
+    let defaulClasses = null;
+    export let items = getItems(field);
 
-  function getItems(field) {
-    let list = [];
-    if (field.extra) {
-      if (field.extra.items) {
-        list = field.extra.items;
-      }
+    function getItems(field) {
+        let list = [];
+        if (field.extra) {
+            if (field.extra.items) {
+                list = field.extra.items;
+            }
+        }
+        return list;
     }
-    return list;
-  }
 
-  // Dispatch.
-  const dispatch = createEventDispatcher();
+    // Dispatch.
+    const dispatch = createEventDispatcher();
 
-  // Change value.
-  function onChangeValue(event) {
-    dispatch('changeValue', {
-      name: field.name,
-      value: event.target.value,
+    // Change value.
+    function onChangeValue(event) {
+        dispatch('changeValue', {
+            name: field.name,
+            value: event.target.value,
+        });
+    }
+
+    // Insert default value.
+    onMount(() => {
+        if (items.length > 0) {
+            dispatch('changeValue', {
+                name: field.name,
+                value: items[0].value,
+            });
+        }
+        if (field.attributes) {
+            if (field.attributes.classes) {
+                classe = field.attributes.classes;
+            }
+        }
     });
-  }
 
-  // Insert default value.
-  onMount(() => {
-    if (items.length > 0) {
-      dispatch('changeValue', {
-        name: field.name,
-        value: items[0].value,
-      });
-    }
-    if (field.attributes) {
-      if (field.attributes.classes) {
-        classe = field.attributes.classes;
-      }
-    }
-  });
-
-  // Lifecycle.
-  afterUpdate(() => {
-    field.value = field.value == undefined ? null : field.value;
-    classe = clsx(classe, defaulClasses);
-    field.attributes = { ...defaultAttributes, ...field.attributes };
-  });
+    // Lifecycle.
+    afterUpdate(() => {
+        field.value = field.value == undefined ? null : field.value;
+        classe = clsx(classe, defaulClasses);
+        field.attributes = {...defaultAttributes, ...field.attributes};
+    });
 </script>
 
-{#each items as item, i}
-  <div
-    class={field.extra.aligne === 'inline'
+{#if field.horizontal_layout }
+    <div>
+        <div class="grid grid-flow-col auto-cols-max justify-around">
+            {#each items as item, i}
+                <div
+                        class="{field.extra.aligne === 'inline'
+      ? 'form-check form-check-inline'
+      : 'form-check'}"
+                >
+                    <input
+                            type="radio"
+                            class="{classe} ml-0 "
+                            id={item.id}
+                            name={field.name}
+                            value={item.value}
+                            checked={item.value === field.value}
+                            on:input={onChangeValue}
+                    />
+                    <span class="ml-0 mr-5">{item.title}</span>
+                </div>
+            {/each}
+        </div>
+    </div>
+{:else}
+    {#each items as item, i}
+        <div
+                class={field.extra.aligne === 'inline'
       ? 'form-check form-check-inline'
       : 'form-check'}
-  >
-    <input
-      type="radio"
-      class={classe}
-      id={item.id}
-      name={field.name}
-      value={item.value}
-      checked={item.value === field.value}
-      on:input={onChangeValue}
-    />
-    <span>{item.title}</span>
-  </div>
-{/each}
+        >
+            <input
+                    type="radio"
+                    class={classe}
+                    id={item.id}
+                    name={field.name}
+                    value={item.value}
+                    checked={item.value === field.value}
+                    on:input={onChangeValue}
+            />
+            <span>{item.title}</span>
+        </div>
+    {/each}
+{/if}

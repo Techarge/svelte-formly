3c9b2f3dbb91bdec169b7ee62050e6a1206b1b3b
Ling Li <ling@techarge.co.uk>
2021-09-23 17:26:22 +0100

    update input class

--- src/Components/Input.svelte
+++ src/Components/Input.svelte
@@ -1,59 +1,95 @@
 <script>
-  import { afterUpdate, createEventDispatcher } from 'svelte';
-  import clsx from 'clsx';
+    import {afterUpdate, createEventDispatcher} from 'svelte';
+    import clsx from 'clsx';
 
-  import { isRequired, scanValue } from '../lib/helpers';
+    import {isRequired, scanValue} from '../lib/helpers';
 
-  // Declar variables.
-  export let field = {};
-  const defaultAttributes = {
-    type: 'text',
-    id: '',
-    classes: '',
-    min: null,
-    max: null,
-    step: null,
-    autocomplete: 'off',
-    placeholder: '',
-    disabled: false,
-    readonly: false,
-  };
-  const fieldAttributes = field.attributes ? field.attributes : {};
-  field.attributes = { ...defaultAttributes, ...fieldAttributes };
+    // Declar variables.
+    export let field = {};
+    const defaultAttributes = {
+        type: 'text',
+        id: '',
+        classes: '',
+        min: null,
+        max: null,
+        step: null,
+        autocomplete: 'off',
+        placeholder: '',
+        disabled: false,
+        readonly: false,
+    };
+    const fieldAttributes = field.attributes ? field.attributes : {};
+    field.attributes = {...defaultAttributes, ...fieldAttributes};
 
-  let classe = null;
-  let defaulClasses = null;
+    let classe = null;
+    let defaulClasses = null;
 
-  // Dispatch.
-  const dispatch = createEventDispatcher();
+    // Dispatch.
+    const dispatch = createEventDispatcher();
 
-  // Change value field.
-  function onChangerValue(event) {
-    dispatch('changeValue', {
-      name: field.name,
-      value: scanValue(field.attributes.type, event.target.value),
+    // Change value field.
+    function onChangerValue(event) {
+        dispatch('changeValue', {
+            name: field.name,
+            value: scanValue(field.attributes.type, event.target.value),
+        });
+    }
+
+    // Lifecycle.
+    afterUpdate(() => {
+        field.value = field.value == undefined ? null : field.value;
     });
-  }
-
-  // Lifecycle.
-  afterUpdate(() => {
-    field.value = field.value == undefined ? null : field.value;
-  });
 </script>
 
-<input
-  type={field.attributes.type}
-  name={field.name}
-  value={field.value}
-  id={field.attributes.id}
-  class={clsx(field.attributes.classes)}
-  placeholder={field.attributes.placeholder}
-  required={isRequired(field)}
-  disabled={field.attributes.disabled}
-  readonly={field.attributes.readonly}
-  min={field.attributes.min}
-  max={field.attributes.max}
-  step={field.attributes.step}
-  autocomplete={field.attributes.autocomplete}
-  on:input={onChangerValue}
-/>
+
+{#if field.attributes.type == "range"}
+    <input
+            type={field.attributes.type}
+            name={field.name}
+            value={field.value}
+            id={field.attributes.id}
+            class={clsx(field.attributes.classes)}
+            placeholder={field.attributes.placeholder}
+            required={isRequired(field)}
+            disabled={field.attributes.disabled}
+            readonly={field.attributes.readonly}
+            min={field.attributes.min}
+            max={field.attributes.max}
+            step={field.attributes.step}
+            autocomplete={field.attributes.autocomplete}
+            list="tickmarks"
+            on:input={onChangerValue}
+    />
+    <datalist id="tickmarks" class="text-black-500">
+        {#each Array(parseInt(field.attributes.max) - parseInt(field.attributes.min) + parseInt(field.attributes.step)) as _, i}
+            <option value="{(i*parseInt(field.attributes.step))+parseInt(field.attributes.min)}"
+                    label="{(i*parseInt(field.attributes.step))+parseInt(field.attributes.min)}"></option>
+        {/each}
+    </datalist>
+{:else}
+    <input
+            type={field.attributes.type}
+            name={field.name}
+            value={field.value}
+            id={field.attributes.id}
+            class={clsx(field.attributes.classes)}
+            placeholder={field.attributes.placeholder}
+            required={isRequired(field)}
+            disabled={field.attributes.disabled}
+            readonly={field.attributes.readonly}
+            min={field.attributes.min}
+            max={field.attributes.max}
+            step={field.attributes.step}
+            autocomplete={field.attributes.autocomplete}
+            on:input={onChangerValue}
+    />
+{/if}
+
+<style>
+    datalist {
+        display: flex;
+        justify-content: space-between;
+        color: grey;
+        width: 100%;
+    }
+</style>

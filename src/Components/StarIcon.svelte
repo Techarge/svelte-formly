89b5721813e4802bebb602f110d5dbdea5779ce6
ZOESCOMPUTER\Zoe <zoeutting@techarge.co.uk>
2021-09-24 12:18:22 +0100

    Star Rating Colouring

--- src/Components/StarIcon.svelte
+++ src/Components/StarIcon.svelte
@@ -9,14 +9,20 @@
 	}
 	.starPath {
 		fill: none;
-		stroke: #999;
+		stroke: #000000;
 	}
 	.filled {
-		fill: yellow;
+		fill: url(#grad2);
 	}
 </style>
 
 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="31" viewBox="0 0 51 48" class="star" on:mouseover on:mouseleave on:click data-starid={starId} let:class>
 	<title>{title}</title>
-	<path class:filled  data-starid={starId} class="starPath" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" />
+	    <defs>
+    <linearGradient id="grad2" x1="0%" y1="0%" x2="0%" y2="100%">
+      <stop offset="0%" style="stop-color:#5AB4F2;stop-opacity:1" />
+      <stop offset="100%" style="stop-color:#00FFE0;stop-opacity:1" />
+    </linearGradient>
+  </defs>
+	<path class:filled data-starid={starId} class="starPath" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" />
 </svg>

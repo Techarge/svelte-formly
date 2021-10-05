936678c336f11187e2e90b2aa30eedced0d70522
ZOESCOMPUTER\Zoe <zoeutting@techarge.co.uk>
2021-10-05 11:14:24 +0100

    Fixing Star widths

--- src/Components/StarIcon.svelte
+++ src/Components/StarIcon.svelte
@@ -6,17 +6,19 @@
 <style>
 	.star {
 		margin-right: 4px;
+        max-width: 80px;
 	}
 	.starPath {
 		fill: #C4C4C4;;
 		stroke: #000000;
+
 	}
 	.filled {
 		fill: url(#grad2) #5AB4F2;
 	}
 </style>
 
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 51 48" class="star w-80px h-80px" on:mouseover on:mouseleave on:click data-starid={starId} let:class>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 51 48" class="star" on:mouseover on:mouseleave on:click data-starid={starId} let:class>
 	<title>{title}</title>
 	    <defs>
     <linearGradient id="grad2" x1="0%" y1="0%" x2="0%" y2="100%">

33255f1e9735d50f8d6ce897dff40ca891d8fb8c
Dominic <dominic.langsdale@techarge.co.uk>
2021-10-06 17:46:36 +0100

    null or empty values don't trigger min or max

--- src/Validation/rules/min.js
+++ src/Validation/rules/min.js
@@ -1,6 +1,17 @@
 export function min (val, args) {
   const minValue = parseFloat(args[0])
+
+  if (
+      val === "" ||
+      val === '' ||
+      val === null ||
+      val === undefined
+  ) {
+    return true
+  }
+
   val = val ? val : ''
+
   const value = isNaN(val) ? val.length : parseFloat(val)
 
   return value >= minValue
